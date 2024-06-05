e3071f23f36edd6e4eab9cccd7c072fcded6b46c
JPRojasVas <Juanparovas3@gmail.com>
2024-06-05 18:38:39 +0200

    feat (AndCriteria.java) : create class AndCriteria and methods

---
+++ Stock-Ascii-Java/src/main/java/edu/badpals/stockx/Criteria/AndCriteria.java
@@ -0,0 +1,24 @@
+package edu.badpals.stockx.Criteria;
+
+import edu.badpals.stockx.item.Item;
+import edu.badpals.stockx.offer.Offer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AndCriteria implements Criteria {
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public AndCriteria(Criteria criteria,Criteria otherCriteria){
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        return criteria.checkCriteria(item).stream()
+                .filter(i->otherCriteria.checkCriteria(item).contains(i))
+                .collect(Collectors.toList());
+    }
+}
