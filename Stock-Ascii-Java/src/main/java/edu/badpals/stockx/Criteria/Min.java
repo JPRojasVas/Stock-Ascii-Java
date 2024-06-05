a8af38a3448f6ceae5e21ef348063af4520b25de
JPRojasVas <Juanparovas3@gmail.com>
2024-06-05 18:28:02 +0200

    feat (Min.java) : create class Min and methods

---
+++ Stock-Ascii-Java/src/main/java/edu/badpals/stockx/Criteria/Min.java
@@ -0,0 +1,26 @@
+package edu.badpals.stockx.Criteria;
+
+import edu.badpals.stockx.item.Item;
+import edu.badpals.stockx.offer.Offer;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Min implements Criteria {
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public Min(Criteria criteria,Criteria otherCriteria){
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        Optional<Offer> min = new AndCriteria(this.criteria,this.otherCriteria)
+                .checkCriteria(item)
+                .stream()
+                .min(Offer::compareTo);
+        return min.isPresent() ? List.of(min.get()) : List.of();
+    }
+}
