cb28130a20416616d6332eff5465dc1833fb809f
JPRojasVas <Juanparovas3@gmail.com>
2024-06-05 18:25:25 +0200

    feat (LastSale.java) : create class LastSale and methods

---
+++ Stock-Ascii-Java/src/main/java/edu/badpals/stockx/Criteria/LastSale.java
@@ -0,0 +1,20 @@
+package edu.badpals.stockx.Criteria;
+
+import edu.badpals.stockx.item.Item;
+import edu.badpals.stockx.offer.Offer;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LastSale implements Criteria {
+    public LastSale(){}
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        List<Offer> salesList = new Sales().checkCriteria(item);
+
+        return salesList.isEmpty()?
+                List.of()
+                : List.of(salesList.getLast());
+    }
+}
