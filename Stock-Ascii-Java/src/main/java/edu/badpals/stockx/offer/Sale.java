19bc8cc2b0007bb93db09d6afde16c54778f6258
JPRojasVas <Juanparovas3@gmail.com>
2024-06-05 18:05:17 +0200

    feat (Sale.java) : create class Sale and add attributes and methods

---
+++ Stock-Ascii-Java/src/main/java/edu/badpals/stockx/offer/Sale.java
@@ -0,0 +1,29 @@
+package edu.badpals.stockx.offer;
+
+public class Sale implements Offer {
+    private String size;
+    private Integer bid;
+
+    public Sale(String size, int bid) {
+        this.bid = bid;
+        this.size = size;
+    }
+
+    public String size() {
+        return this.size;
+    }
+
+    public int value() {
+        return this.bid;
+    }
+
+    @Override
+    public int compareTo(Offer offer) {
+        return this.bid.compareTo(offer.value());
+    }
+
+    @Override
+    public String toString() {
+        return "\t\t" + size() + "\t\t" + value() + "\n";
+    }
+}
